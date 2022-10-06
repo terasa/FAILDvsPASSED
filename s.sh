7dca44bd0fb2be345f81f955a985e2f7cdeadcff
gazeta.pl <teresa.rajda@gmail.com>
2022-10-06 10:11:22 +0200

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,2 +1,2 @@
 echo "Test: /usr/share/zfs/zfs-tests/tests/functional/cli_root/zpool_create/zpool_create_features_001_pos (run as root) [00:01] [PASS]"
-echo echo "Test: /usr/share/zfs/zfs-tests/tests/functional/cli_root/zpool_create/zpool_create_features_001_pos (run as root) [00:01] \u001b[34m[PASS]\u001b[0m"  
+echo "Test: /usr/share/zfs/zfs-tests/tests/functional/cli_root/zpool_create/zpool_create_features_001_pos (run as root) [00:01] \u001b[34m[PASS]\u001b[0m"  
